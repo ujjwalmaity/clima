7d112a025ce59488da8850ab48c4482deb198bd2
ujjwal <ujjwalmaity98@gmail.com>
2020-05-11 23:33:04 +0530

    Creating and Styling a TextField Widget for Text Entry

--- lib/utilities/constants.dart
+++ lib/utilities/constants.dart
@@ -24,3 +24,22 @@ const kSpinKit = SpinKitDoubleBounce(
   color: Colors.white,
   size: 100.0,
 );
+
+const kTextFieldInputDecoration = InputDecoration(
+  filled: true,
+  fillColor: Colors.white,
+  icon: Icon(
+    Icons.location_city,
+    color: Colors.white,
+  ),
+  hintText: 'Enter city name',
+  hintStyle: TextStyle(
+    color: Colors.grey,
+  ),
+  border: OutlineInputBorder(
+    borderRadius: BorderRadius.all(
+      Radius.circular(10.0),
+    ),
+    borderSide: BorderSide.none,
+  ),
+);
